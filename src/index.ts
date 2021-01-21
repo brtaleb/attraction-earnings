dbbd3acaf55263dfe4f96bf668a0c78b82ba58be
Brahim <brahim@ezus.io>
2021-01-21 17:57:29 +0100

    Final Answer

--- src/index.ts
+++ src/index.ts
@@ -7,39 +7,53 @@ var inputs: string[] = readline().split(' ');
 const L: number = parseInt(inputs[0]);
 const C: number = parseInt(inputs[1]);
 const N: number = parseInt(inputs[2]);
-let queue: number[] = [];
+let P: number[] = [];
 
 for (let i = 0; i < N; i++) {
     const pi: number = parseInt(readline());
-    queue.push(pi);
+    P.push(pi);
 }
 
+let groups: number[] = [];
+let sequences: string[] = [P.join()];
+
+let group: number | undefined = P.shift();
+let offset: number | undefined = undefined;
+
 let res = 0;
-let runs = C;
 
-while(runs > 0){
-    let people: number[] = [];
+while(offset === undefined || offset < 0){
     let peopleSum = 0;
-    let tmp: number[] = []
 
-    while(peopleSum < L){
-        let group = queue[0];
-
-
-        if(group+peopleSum <= L) {
-            queue.shift();
-            people.push(group);
+    for(let i=0; i<=P.length; i++){
+        if(group && peopleSum + group <= L){
+            P.push(group);
             peopleSum += group;
-            tmp.push(group);
+            group = P.shift();
+
         }
         else break;
     }
 
-    res += people.reduce((total: number, curr: number) => total+=curr, 0)
-    queue = queue.concat(tmp);
-    runs--;
+    offset = sequences.indexOf(P.join()); // find cycle offset
+    sequences.push(P.join()); // save sequence
+    groups.push(peopleSum);
 }
 
+// profit of the group sequences that happen once
+let it = 0;
+while(it < offset){
+    res+= groups[it];
+    it++;
+}
+
+// profit of the rest
+let rest = (C - offset) % (groups.length - offset);
+while(it < groups.length){
+    res += groups[it] * (Math.floor((C - offset) / (groups.length - offset)) + (rest > 0 ? 1 : 0));
+    rest--;
+    it++;
+}
 // Write an answer using console.log()
 // To debug: console.error('Debug messages...');
 
